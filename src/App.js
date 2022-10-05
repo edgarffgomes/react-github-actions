243e66ae6ac7cb08c8af0534ab6777cdaff07e7d
Edgar Gomes <57186828+edgarffgomes@users.noreply.github.com>
2022-10-05 13:42:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Raphael Collin
+          Edgar Gomes
         </p>
         <a
           className="App-link"
